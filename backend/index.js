b4910b5ccae0068cbabb300ea33b100bf9920c09
Vimal Negi <109637181+Vimalnegi03@users.noreply.github.com>
2025-08-04 17:48:10 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ import cors from "cors";
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://internship-1-l5aa.onrender.com",
   credentials: true    // JavaScript boolean is lowercase
 }));
 
